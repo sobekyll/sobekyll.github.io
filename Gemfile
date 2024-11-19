5cddcd3a9e07a29999add75e562f75bf1dd5df00
boybeak <boybeak@gmail.com>
2024-11-19 21:33:51 +0800

    initial commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+
+gem "jekyll", "~> 4.3.0"  # 指定 Jekyll 的版本
+gem "webrick", "~> 1.7"   # 解决 Jekyll serve 在某些环境下的依赖问题
+gem "jekyll-paginate"
---
+++ _site/assets/js/sobekyll.js
@@ -0,0 +1,8 @@
+onPageLoad();
+function onPageLoad() {
+    // document.addEventListener("DOMContentLoaded", (event) => {
+    //     document.querySelectorAll("pre code").forEach((block) => {
+    //         hljs.highlightElement(block);
+    //     });
+    // });
+}
